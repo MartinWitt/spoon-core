dfcfb7130f1643d04a8849b83b788cc639c1524a
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-12-24 16:49:04 +0100

    ✨ Rebuild History of last 10 commits

---
+++ readme.md
@@ -0,0 +1,10 @@
+# SpoonRebuilder
+
+This is a simple tool to rebuild the Spoon repository without generics.
+
+## Motivation
+## Usage
+### Gradle
+### Maven
+## License
+## SniperPrinting Fixes 
---
+++ src/test/java/io/github/martinwitt/spoonrebuilder/github/GitHubUtilsTest.java
@@ -0,0 +1,24 @@
+package io.github.martinwitt.spoonrebuilder.github;
+
+import org.eclipse.jgit.lib.Ref;
+import org.junit.jupiter.api.Test;
+
+public class GitHubUtilsTest {
+    @Test
+    void testGetTagsOfRepo() {
+        var set = GitHubUtils.getTagsOfRepo("https://github.com/martinwitt/spoon-core").stream()
+                .map(this::getCommitHashOfTag)
+                .toList();
+        var list = GitHubUtils.getCommitsOfRepo("https://github.com/INRIA/spoon", 20);
+
+        // for (RevCommit revCommit : list) {
+        //   System.out.println(revCommit.getName());
+        // }
+        var newCommits = list.stream().filter(v -> !set.contains(v.getName())).toList();
+        newCommits.forEach(System.out::println);
+    }
+
+    private String getCommitHashOfTag(Ref revCommit) {
+        return revCommit.getName().split("/")[2];
+    }
+}
