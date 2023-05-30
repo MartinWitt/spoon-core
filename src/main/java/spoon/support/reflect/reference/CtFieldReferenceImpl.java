8d43fd6e36f15f9fc7857ff211e5a42dabdefd69
? <?@9c81a061fcdb>
2023-05-30 01:49:20 +0000

    Rebuild Spoon: 093b49747b1ae86be55ca886da30b307ac6e64d2

--- src/main/java/spoon/support/reflect/reference/CtFieldReferenceImpl.java
+++ src/main/java/spoon/support/reflect/reference/CtFieldReferenceImpl.java
@@ -1,9 +1,9 @@
 /*
  * SPDX-License-Identifier: (MIT OR CECILL-C)
  *
- * Copyright (C) 2006-2019 INRIA and contributors
+ * Copyright (C) 2006-2023 INRIA and contributors
  *
- * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
+ * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) or the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
  */
 package spoon.support.reflect.reference;
 
@@ -171,6 +171,12 @@ public class CtFieldReferenceImpl extends CtVariableReferenceImpl implements CtF
 
 	@Override
 	public Set<ModifierKind> getModifiers() {
+		// special-case the length field of array, as it doesn't have a declaration
+		// as arrays only have one field, we do not need to check the name additionally
+		CtTypeReference declaringType = getDeclaringType();
+		if (declaringType != null && declaringType.isArray()) {
+			return Set.of(ModifierKind.PUBLIC, ModifierKind.FINAL);
+		}
 		CtVariable v = getDeclaration();
 		if (v != null) {
 			return v.getModifiers();
